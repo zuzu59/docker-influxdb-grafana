39f8ea2f8aaf4c584b620fb7f053c6ae9ac0efc0
Christian Zufferey <christian@zufferey.com>
2019-10-08 15:25:02 +0200

    Terminé, enfin, mes scripts de tests pour tester l'interface d'enregistrement des durées des scripts. Cela marche bien :-)

--- test_batch_script.sh
+++ test_batch_script.sh
@@ -1,44 +1,27 @@
 #!/bin/bash
-#Petit script pour enregistrer des durées d'exécution de scripts dans InfluxDB
-#zf191008.1119
+#Petit script pour tester l'enregistrement des durées d'exécution de scripts dans InfluxDB
+#zf191008.1506
 
 
-echo "start...."
-t1=$(date +%s%N)
-sleep 1.5
-t2=$(date +%s%N)
-echo "stop..."
-
-t21=$(echo "($t2 - $t1)/1000000000" | bc -l)
-
-
-echo "start envoi..."
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=test1 state_z=1 $t1"
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=test1 state_z=1 $t2"
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=test1 time_duration=$t21"
-echo "stop envoi..."
+echo "start...test_batch_script"
 
 
 
-echo -e "
+for i in {1..5}
+do
+    ./test_record_script.sh test$i 0 $(echo "5*$RANDOM/32767" | bc -l) &
+done
 
-Le script a commencé à $(date -d @$(echo "$t1/1000000000" |bc -l))
+sleep 6
 
-Il a termniné à $(date -d @$(echo "$t2/1000000000" |bc -l))
-
-
-Et a duré $t21 secondes
-
-"
-
-exit
+for i in {1..5}
+do
+    ./test_record_script.sh test$i 1.0 $(echo "5*$RANDOM/32767" | bc -l) &
+done
 
 
 
 
-p=$(echo "1 + $RANDOM % 10" | bc)
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "energy,compteur=2 puissance=$p"
-sleep 3
-
+echo "stop...test_batch_script"
 
 
--- test_record_script.sh
+++ test_record_script.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 #Petit script pour enregistrer des durées d'exécution de scripts dans InfluxDB
-#zf191008.1119
+#zf191008.1508
 
 #test si l'argument est vide
 if [ -z "$1" ]
   then
-    echo -e "\nUsage: ./test_record_script.sh nom_du_test durée_du_test \n\n"
+    echo -e "\nUsage: ./test_record_script.sh state_val nom_du_test durée_du_test \n\n"
     exit
 fi
 
+#sleep $(echo "5*$RANDOM/32767" | bc -l)
 
 echo "start....$1"
 t1=$(date +%s%N)
-sleep $2
+sleep $3
 t2=$(date +%s%N)
-echo "stop...$1"
+#echo "stop...$1"
 
 t21=$(echo "($t2 - $t1)/1000000000" | bc -l)
 
 
-echo "start envoi..."
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=test1 state_z=1 $t1"
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=test1 state_z=1 $t2"
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=test1 time_duration=$t21"
-echo "stop envoi..."
+#echo "start envoi..."
+curl -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=$1 state_z=$2 $t1" &
+curl -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=$1 state_z=$(echo "$2+1" |bc -l) $t2" &
+curl -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "scripts,test=$1 time_duration=$t21" &
+#echo "stop envoi..."
 
 
 
 echo -e "
 
-Le script a commencé à $(date -d @$(echo "$t1/1000000000" |bc -l))
+Le script $1 a commencé à $(date -d @$(echo "$t1/1000000000" |bc -l))
 
 Il a termniné à $(date -d @$(echo "$t2/1000000000" |bc -l))
 
